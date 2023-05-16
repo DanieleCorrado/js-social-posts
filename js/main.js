4b2d6e34851eeff1fea1f2448f0c6ee01160260c
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-16 16:53:17 +0200

    add post feed

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=24"
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -55,3 +55,158 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById("container");
+
+posts.forEach(element => {
+
+    // Creo il div per il post e gli aggiungo la classe post
+
+    const post = document.createElement("div");
+    post.classList.add("post");
+
+    // Creo il div per l'header post gli aggiungo la classe post__header
+
+    const postHeader = document.createElement("div");
+    postHeader.classList.add("post__header");
+
+    post.appendChild(postHeader);
+
+    // Creo il div post-meta le informazioni dell'utente e gli aggiungo la classe post-meta
+
+    const postMeta = document.createElement("div");
+    postMeta.classList.add("post-meta");
+
+    postHeader.appendChild(postMeta);
+
+    // Creo il div per l'icona dell'utente e gli aggiungo la classe post-meta__icon
+
+    const postMetaIcon = document.createElement("div");
+    postMetaIcon.classList.add("post-meta__icon");
+
+    postMeta.appendChild(postMetaIcon);
+
+    // Creo l'elemento img per inserire l'immagine e gli aggiungo la classe profile-pic
+
+    const profilePic = document.createElement("img");
+    profilePic.classList.add("profile-pic");
+    profilePic.src = element.author.image;
+
+    postMetaIcon.appendChild(profilePic);
+
+    // Creo il div per nomee utente e data pubblicazione post e gli aggiungo la classe post-meta__data
+
+    const postMetaData = document.createElement("div");
+    postMetaData.classList.add("post-meta__data");
+
+    postMeta.appendChild(postMetaData);
+
+    // Creo il div per il nome dell'utente e gli aggiungo la classe post-meta__author
+
+    const postMetaAuthor = document.createElement("div");
+    postMetaAuthor.classList.add("post-meta__author");
+    postMetaAuthor.innerText = element.author.name;
+    postMetaData.appendChild(postMetaAuthor);
+
+    // Creo il div per la data di pubblicazione e gli aggiungo la classe post-meta__time
+
+    const postMetaTime = document.createElement("div");
+    postMetaTime.classList.add("post-meta__time");
+    postMetaTime.innerText= element.created;
+    postMetaData.appendChild(postMetaTime);
+    
+    // Creo il div per la desrizione del post e gli aggiungo la classe post__text
+
+    const postText = document.createElement("div");
+    postText.classList.add("post__text");
+    postText.innerHTML = element.content;
+    post.appendChild(postText);
+
+    // Creo il div per l'immagine del post e gli aggiungo la classe post__image
+
+    const postImage = document.createElement("div");
+    postImage.classList.add("post__image");
+
+    post.appendChild(postImage);
+
+    // Creo l'elemento ing per inserire l'immagine del post
+
+    const image = document.createElement("img");
+    image.src = element.media;
+
+    postImage.appendChild(image);
+
+    // Creo il div per il footer del post e gli aggiungo la classe post__footer
+
+    const postFooter = document.createElement("div");
+    postFooter.classList.add("post__footer");
+
+    post.appendChild(postFooter);
+
+    // Creo il div per i like del post e gli aggiungo la classe likes
+
+    const likes = document.createElement("div");
+    likes.classList.add("likes");
+
+    postFooter.appendChild(likes);
+
+    // Creo il div per il like cta del post e gli aggiungo la classe likes__cta
+
+    const likesCta = document.createElement("div");
+    likesCta.classList.add("likes__cta");
+
+    likes.appendChild(likesCta);
+
+    // Creo l'a per il pulsante like del post e gli aggiungo la classe like-button
+    
+    const likeButton = document.createElement("a");
+    likeButton.classList.add("like-button");
+
+    likesCta.appendChild(likeButton);
+
+    // Creo l'i per l'icona like del post e gli aggiungo le classi like-button__icon, fas fa-thumbs-up
+
+    const likeButtonIcon = document.createElement("i");
+    likeButtonIcon.classList.add("like-button__icon");
+    likeButtonIcon.classList.add("fas");
+    likeButtonIcon.classList.add("fa-thumbs-up");
+
+    likeButton.appendChild(likeButtonIcon);
+
+    // Creo lo span per la label like del post e gli aggiungo la classe like-button__label
+
+    const likeButtonLabel = document.createElement("span");
+    likeButtonLabel.classList.add("like-button__label");
+
+    const like = document.createTextNode(" Mi piace");
+
+    likeButtonLabel.appendChild(like);
+
+    likeButton.appendChild(likeButtonLabel);
+
+
+    // Creo il div per il counter dei like del post e gli aggiungo la classe likes__counter
+
+    const likeCounter = document.createElement("div");
+    likeCounter.classList.add("likes__counter");
+
+    likes.appendChild(likeCounter);
+
+    const startSentence = document.createTextNode("Piace a ");
+
+    likeCounter.appendChild(startSentence);
+
+    const likeNuber = document.createElement("b");
+    likeNuber.classList.add("js-likes-counter");
+    likeNuber.innerHTML = element.likes;
+
+    likeCounter.appendChild(likeNuber)
+
+    const endSentence = document.createTextNode(" persone");
+
+    likeCounter.appendChild(endSentence);
+
+    container.appendChild(post);
+
+});
+
